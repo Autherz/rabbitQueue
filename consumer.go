aa7353953b57b00d4fd11ff3a693b2f87deac1f6
atisit thongbai <bom_144440@outlook.co.th>
2019-08-07 12:59:53 +0700

    add transVideos struct

--- consumer.go
+++ consumer.go
@@ -3,8 +3,17 @@ package rabbitQueue
 import (
 	"github.com/streadway/amqp"
 )
+type TransVideos struct {
+	Input          string `json:input`
+	Output         string `json:output`
+	Quality        string `json:quality`
+	PathP          string `json:path`
+	UserId         string `json:userid`
+	MaterialPackId string `json:materialpackid`
+	MaterialId     string `json:materialid`
+}
 
 type Consumer interface {
 	Consume(delivery amqp.Delivery)
-	Transcode(inputPath string, outputPath string, resolution string, socketIoFlag bool) error
+	Transcode(transVideos *TransVideos,inputPath string, outputPath string, socketIoFlag bool) error
 }
